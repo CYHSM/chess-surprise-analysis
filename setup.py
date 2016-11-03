262d4f077f932354d1b9e4e1782b83137659f285
Marx <Markus@frey1.de>
2016-11-03 22:24:39 +0100

    Added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'csa',
+  packages = ['csa'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Analysis of chess games to find surprising moves',
+  author = 'Markus frey',
+  author_email = 'markus.frey1@gmail.com',
+  url = 'https://github.com/CYHSM/chess-surprise-analysis', # use the URL to the github repo
+  #download_url = 'https://github.com/CYHSM/chess-surprise-analysis/tarball/0.1', #
+  keywords = ['chess', 'data analysis'], # arbitrary keywords
+  classifiers = [],
+)
