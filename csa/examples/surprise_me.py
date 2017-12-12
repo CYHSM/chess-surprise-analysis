9e9a24ad815aba204f5b893876c0c8ed9169bf16
CYHSM <markus.frey1@gmail.com>
2017-12-12 09:33:28 +0100

    Added more comprehensive example

--- csa/examples/surprise_me.py
+++ csa/examples/surprise_me.py
@@ -4,13 +4,20 @@ Example code for evaluating a game and finding surprising moves
 from csa import csa
 
 # Load Game from PGN
-path_to_pgn = 'wei_yi_bruzon_batista_2015.pgn'
+path_to_pgn = './games/alphazero-vs-stockfish_game10.pgn'
 chess_game = csa.load_game_from_pgn(path_to_pgn)
 
 # Evaluate Game
+depths = range(1, 37)
 cp, nodes = csa.evaluate_game(chess_game, reset_engine=True,
-                              halfmove_numbers=None, depths=range(1, 35),
+                              halfmove_numbers=None, depths=depths,
                               verbose=1, async_callback=True)
+# Save cp
+csa.save_evaluation(cp, nodes, depths, True,
+                    True, 'alphazero')
+
+# Plot heatmap
+csa.plot_cp(cp, fn='alphazero.svg', save=True)
 
 # Find surprising moves
 ss_df, infos = csa.analyse_evaluations(cp, low=12, high=22)
