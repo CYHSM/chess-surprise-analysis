5fcf8b7ffddea60e391c4035ffc0af95e9ea708b
CYHSM <markus.frey1@gmail.com>
2017-12-20 21:31:21 +0100

    Added avaluations for game 2

--- csa/examples/surprise_me.py
+++ csa/examples/surprise_me.py
@@ -4,7 +4,7 @@ Example code for evaluating a game and finding surprising moves
 from csa import csa
 
 # Load Game from PGN
-path_to_pgn = './games/alphazero-vs-stockfish_game10.pgn'
+path_to_pgn = './games/alphazero-vs-stockfish_game2.pgn'
 chess_game = csa.load_game_from_pgn(path_to_pgn)
 
 # Evaluate Game
@@ -14,10 +14,10 @@ cp, nodes = csa.evaluate_game(chess_game, reset_engine=True,
                               verbose=1, async_callback=True)
 # Save cp
 csa.save_evaluation(cp, nodes, depths, True,
-                    True, 'alphazero_stockfish-111217')
+                    True, 'alphazero_stockfish_game2')
 
 # Plot heatmap
-csa.plot_cp(cp, fn='alphazero_stockfish-111217.svg', save=True)
+csa.plot_cp(cp, fn='alphazero_stockfish_game2.svg', save=True)
 
 # Find surprising moves
 ss_df, infos = csa.analyse_evaluations(cp, low=12, high=22)
